7f9ebe576ceeebe0ad6a60420e3bdae28b74f229
m0satron <msebdani@gmail.com>
2025-07-08 14:34:09 +0200

    refactor: types

---
+++ types/directus/Case.ts
@@ -0,0 +1,13 @@
+import type { Block } from '../components';
+import type { DirectusBuildingBlock } from './BuildingBlock';
+
+export interface Case {
+  id: number;
+  title: Record<string, string>;
+  description: Record<string, string>;
+  content?: Record<string, string>;
+  building_blocks_used?: DirectusBuildingBlock;
+  alternative_cases?: Case[];
+}
+
+export type Cases = Case[]; 
