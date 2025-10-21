d337582a0f66ce38086b4ea81c130d6f3e407d7e
m0satron <msebdani@gmail.com>
2025-10-21 10:47:57 +0200

    feat: component updates for quickscan

--- components/AllyButton.vue
+++ components/AllyButton.vue
@@ -3,105 +3,117 @@
     :class="[
       'ally-btn',
       `ally-btn--${variant}`,
-      { 'ally-btn--block': block, 'ally-btn--disabled': disabled }
+      { 'ally-btn--block': block, 'ally-btn--disabled': disabled },
     ]"
     :disabled="disabled"
   >
     <template v-if="!block && prependIcon">
-      <img
-        class="ally-btn__icon ally-btn__icon--prepend"
-        :src="iconPath(prependIcon)"
-        :alt="`${prependIcon} icon`"
-        aria-hidden="true"
-      >
+      <Transition name="fade-scale" mode="out-in">
+        <img
+          :key="prependIcon"
+          class="ally-btn__icon ally-btn__icon--prepend"
+          :src="iconPath(prependIcon)"
+          :alt="`${prependIcon} icon`"
+          aria-hidden="true"
+        >
+      </Transition>
     </template>
+
     <span class="ally-btn__content">
       {{ label }}
     </span>
+
     <template v-if="!block && appendIcon">
-      <img
-        class="ally-btn__icon ally-btn__icon--append"
-        :src="iconPath(appendIcon)"
-        :alt="`${appendIcon} icon`"
-        aria-hidden="true"
-      >
+      <Transition name="fade-scale" mode="out-in">
+        <img
+          :key="appendIcon"
+          class="ally-btn__icon ally-btn__icon--append"
+          :src="iconPath(appendIcon)"
+          :alt="`${appendIcon} icon`"
+          aria-hidden="true"
+        >
+      </Transition>
     </template>
   </button>
 </template>
 
 <script setup lang="ts">
 // Import all icons in assets/icons
-import ArrowRight from '~/assets/icons/arrow-right.svg';
-import ArrowRightNew from '~/assets/icons/arrow-right-new.svg';
-import ArrowRightPurple from '~/assets/icons/arrow-right-purple.svg';
-import ChevronDown from '~/assets/icons/chevron-down.svg';
-import ClockIcon from '~/assets/icons/clock-icon.svg';
-import ClockIconBlack from '~/assets/icons/clock-icon-black.svg';
-import CloseButton from '~/assets/icons/close-button.svg';
-import LanguageIcon from '~/assets/icons/language-icon.svg';
-import MoneyIcon from '~/assets/icons/money-icon.svg';
-import MoneyIconBlack from '~/assets/icons/money-icon-black.svg';
-import Search from '~/assets/icons/search.svg';
-import ThunderIcon from '~/assets/icons/thunder-icon.svg';
-import ThunderIconBlack from '~/assets/icons/thunder-icon-black.svg';
-import CopyLink from '~/assets/icons/copy-link.svg';
-import Check from '~/assets/icons/check.svg';
+import ArrowRight from "~/assets/icons/arrow-right.svg";
+import ArrowRightNew from "~/assets/icons/arrow-right-new.svg";
+import ArrowRightPurple from "~/assets/icons/arrow-right-purple.svg";
+import ChevronDown from "~/assets/icons/chevron-down.svg";
+import ClockIcon from "~/assets/icons/clock-icon.svg";
+import ClockIconBlack from "~/assets/icons/clock-icon-black.svg";
+import CloseButton from "~/assets/icons/close-button.svg";
+import LanguageIcon from "~/assets/icons/language-icon.svg";
+import MoneyIcon from "~/assets/icons/money-icon.svg";
+import MoneyIconBlack from "~/assets/icons/money-icon-black.svg";
+import Search from "~/assets/icons/search.svg";
+import ThunderIcon from "~/assets/icons/thunder-icon.svg";
+import ThunderIconBlack from "~/assets/icons/thunder-icon-black.svg";
+import CopyLink from "~/assets/icons/copy-link.svg";
+import Check from "~/assets/icons/check.svg";
 
 defineProps({
   prependIcon: {
-    type: String as () => 'arrow-right' | 'search' | 'close' | 'chevron-down' | 'copy-link' | 'check' | string,
-    default: ''
+    type: String as () =>
+      | "arrow-right"
+      | "search"
+      | "close"
+      | "chevron-down"
+      | "copy-link"
+      | "check"
+      | string,
+    default: "",
   },
   appendIcon: {
-    type: String as () => 'arrow-right' | 'search' | 'close' | 'chevron-down' | 'copy-link' | 'check' | string,
-    default: ''
+    type: String as () =>
+      | "arrow-right"
+      | "search"
+      | "close"
+      | "chevron-down"
+      | "copy-link"
+      | "check"
+      | string,
+    default: "",
   },
-  block: {
-    type: Boolean,
-    default: false
-  },
+  block: { type: Boolean, default: false },
   variant: {
-    type: String as () => 'primary' | 'secondary' | 'gray',
-    default: 'primary'
+    type: String as () => "primary" | "secondary" | "gray",
+    default: "primary",
   },
-  disabled: {
-    type: Boolean,
-    default: false
-  },
-  label: {
-    type: String,
-    required: true
-  }
+  disabled: { type: Boolean, default: false },
+  label: { type: String, required: true },
 });
 
 const iconMap: Record<string, string> = {
-  'arrow-right': ArrowRight,
-  'arrow-right-new': ArrowRightNew,
-  'arrow-right-purple': ArrowRightPurple,
-  'chevron-down': ChevronDown,
-  'clock-icon': ClockIcon,
-  'clock-icon-black': ClockIconBlack,
-  'close': CloseButton,
-  'close-button': CloseButton,
-  'language-icon': LanguageIcon,
-  'money-icon': MoneyIcon,
-  'money-icon-black': MoneyIconBlack,
-  'search': Search,
-  'thunder-icon': ThunderIcon,
-  'thunder-icon-black': ThunderIconBlack,
-  'copy-link': CopyLink,
-  'check': Check
+  "arrow-right": ArrowRight,
+  "arrow-right-new": ArrowRightNew,
+  "arrow-right-purple": ArrowRightPurple,
+  "chevron-down": ChevronDown,
+  "clock-icon": ClockIcon,
+  "clock-icon-black": ClockIconBlack,
+  close: CloseButton,
+  "close-button": CloseButton,
+  "language-icon": LanguageIcon,
+  "money-icon": MoneyIcon,
+  "money-icon-black": MoneyIconBlack,
+  search: Search,
+  "thunder-icon": ThunderIcon,
+  "thunder-icon-black": ThunderIconBlack,
+  "copy-link": CopyLink,
+  check: Check,
 };
 
 function iconPath(name: string) {
-  return iconMap[name] || '';
+  return iconMap[name] || "";
 }
-
 </script>
 
 <style scoped lang="scss">
-@use '/assets/scss/colors';
-@use 'sass:map';
+@use "/assets/scss/colors";
+@use "sass:map";
 
 .ally-btn {
   display: inline-flex;
@@ -111,11 +123,15 @@ function iconPath(name: string) {
   font-weight: 600;
   border-radius: 0.5rem;
   padding: 0.75rem 1.5rem;
-  border: 1.5px solid map.get(colors.$colors, 'gray-light-mode-300');
-  background: map.get(colors.$colors, 'base-white');
-  color: map.get(colors.$colors, 'gray-light-mode-950');
+  border: 1.5px solid map.get(colors.$colors, "gray-light-mode-300");
+  background: map.get(colors.$colors, "base-white");
+  color: map.get(colors.$colors, "gray-light-mode-950");
   cursor: pointer;
-  transition: background 0.15s, color 0.15s, border 0.15s;
+  transition:
+    background 0.15s,
+    color 0.15s,
+    border 0.15s,
+    transform 0.15s;
   outline: none;
   user-select: none;
 
@@ -124,40 +140,55 @@ function iconPath(name: string) {
     height: 1.25em;
     display: inline-block;
     vertical-align: middle;
-    &--prepend { margin-right: 0.5em; }
-    &--append { margin-left: 0.5em; }
+    &--prepend {
+      margin-right: 0.5em;
+    }
+    &--append {
+      margin-left: 0.5em;
+    }
+  }
+
+  &:active:not(:disabled) {
+    transform: scale(0.97);
   }
 
   &--primary {
-    background: map.get(colors.$colors, 'base-white');
-    color: map.get(colors.$colors, 'gray-light-mode-950');
-    border: 1.5px solid map.get(colors.$colors, 'gray-light-mode-300');
-    &:hover:not(:disabled), &:active:not(:disabled) {
-      background: map.get(colors.$colors, 'brand-300');
-      color: map.get(colors.$colors, 'gray-light-mode-950');
-      border-color: map.get(colors.$colors, 'brand-300');
+    background: map.get(colors.$colors, "base-white");
+    color: map.get(colors.$colors, "gray-light-mode-950");
+    border: 1.5px solid map.get(colors.$colors, "gray-light-mode-300");
+    &:hover:not(:disabled),
+    &:active:not(:disabled) {
+      background: map.get(colors.$colors, "brand-300");
+      color: map.get(colors.$colors, "gray-light-mode-950");
+      border-color: map.get(colors.$colors, "brand-300");
     }
   }
 
   &--secondary {
-    background: map.get(colors.$colors, 'brand-600');
-    color: map.get(colors.$colors, 'base-white');
-    border: 1.5px solid map.get(colors.$colors, 'brand-600');
-    &:hover:not(:disabled), &:active:not(:disabled) {
-      background: map.get(colors.$colors, 'brand-300');
-      color: map.get(colors.$colors, 'base-white');
-      border-color: map.get(colors.$colors, 'brand-300');
+    background: map.get(colors.$colors, "brand-600");
+    color: map.get(colors.$colors, "base-white");
+    border: 1.5px solid map.get(colors.$colors, "brand-600");
+    &:hover:not(:disabled),
+    &:active:not(:disabled) {
+      background: map.get(colors.$colors, "brand-300");
+      color: map.get(colors.$colors, "base-white");
+      border-color: map.get(colors.$colors, "brand-300");
+    }
+
+    .ally-btn__icon {
+      filter: brightness(0) invert(1);
     }
   }
 
   &--gray {
-    background: map.get(colors.$colors, 'gray-light-mode-100');
-    color: map.get(colors.$colors, 'gray-light-mode-950');
-    border: 1.5px solid map.get(colors.$colors, 'gray-light-mode-100');
-    &:hover:not(:disabled), &:active:not(:disabled) {
-      background: map.get(colors.$colors, 'brand-300');
-      color: map.get(colors.$colors, 'base-white');
-      border-color: map.get(colors.$colors, 'brand-300');
+    background: map.get(colors.$colors, "gray-light-mode-100");
+    color: map.get(colors.$colors, "gray-light-mode-950");
+    border: 1.5px solid map.get(colors.$colors, "gray-light-mode-100");
+    &:hover:not(:disabled),
+    &:active:not(:disabled) {
+      background: map.get(colors.$colors, "brand-300");
+      color: map.get(colors.$colors, "base-white");
+      border-color: map.get(colors.$colors, "brand-300");
     }
   }
 
@@ -165,7 +196,9 @@ function iconPath(name: string) {
     display: flex;
     width: 100%;
     justify-content: center;
-    .ally-btn__icon { display: none; }
+    .ally-btn__icon {
+      display: none;
+    }
   }
 
   &--disabled,
@@ -175,15 +208,29 @@ function iconPath(name: string) {
   }
   &--primary:disabled,
   &--primary.ally-btn--disabled {
-    background: map.get(colors.$colors, 'gray-light-mode-100');
-    color: map.get(colors.$colors, 'gray-light-mode-500');
-    border-color: map.get(colors.$colors, 'gray-light-mode-200');
+    background: map.get(colors.$colors, "gray-light-mode-100");
+    color: map.get(colors.$colors, "gray-light-mode-500");
+    border-color: map.get(colors.$colors, "gray-light-mode-200");
   }
   &--secondary:disabled,
   &--secondary.ally-btn--disabled {
-    background: map.get(colors.$colors, 'brand-100');
-    color: map.get(colors.$colors, 'gray-light-mode-400');
-    border-color: map.get(colors.$colors, 'gray-light-mode-200');
+    background: map.get(colors.$colors, "brand-100");
+    color: map.get(colors.$colors, "gray-light-mode-400");
+    border-color: map.get(colors.$colors, "gray-light-mode-200");
   }
 }
-</style> 
+
+/* --- Icon fade/scale animation --- */
+.fade-scale-enter-active,
+.fade-scale-leave-active {
+  transition: all 0.25s ease;
+}
+.fade-scale-enter-from {
+  opacity: 0;
+  transform: scale(0.6);
+}
+.fade-scale-leave-to {
+  opacity: 0;
+  transform: scale(1.4);
+}
+</style>
--- components/WhyAlly.vue
+++ components/WhyAlly.vue
@@ -1,52 +1,55 @@
 <template>
-      <div class="why-ally-container ">
-      <h2 class="why-ally-title">Why should you use Ally?</h2>
-      <p class="why-ally-description">Choose your path to action: run a quick scan to identify your next steps or join a workshop to co-create your responsible AI strategy.</p>
-   
+  <div class="why-ally-container">
+    <h2 class="why-ally-title">Why should you use Ally?</h2>
+    <p class="why-ally-description">
+      Choose your path to action: run a quick scan to identify your next steps
+      or join a workshop to co-create your responsible AI strategy.
+    </p>
+
     <section class="why-ally-sections">
-      <div v-for="(section, idx) in whyAllySections" :key="idx" class="why-ally-section">
+      <div
+        v-for="(section, idx) in whyAllySections"
+        :key="idx"
+        class="why-ally-section"
+      >
         <div class="icon-container bg-color-brand-200">
-          <img :src="section.icon" :alt="section.alt" >
+          <img :src="section.icon" :alt="section.alt" />
         </div>
         <h2>{{ section.title }}</h2>
         <p>{{ section.text }}</p>
       </div>
     </section>
   </div>
-
 </template>
 
 <script setup lang="ts">
-import successIcon from '@/assets/icons/about/success.svg';
-import trustIcon from '@/assets/icons/about/trust.svg';
-import solutionsIcon from '@/assets/icons/about/solutions.svg';
-
+import successIcon from "@/assets/icons/about/success.svg";
+import trustIcon from "@/assets/icons/about/trust.svg";
+import solutionsIcon from "@/assets/icons/about/solutions.svg";
 
 const whyAllySections = [
   {
     icon: successIcon,
-    alt: 'Pillars',
-    title: 'Responsible AI Success',
-    text: 'ALLY boosts your chances of designing, developing, implementing, and using responsible AI innovations.',
+    alt: "Pillars",
+    title: "Responsible AI Success",
+    text: "ALLY boosts your chances of designing, developing, implementing, and using responsible AI innovations.",
   },
   {
     icon: trustIcon,
-    alt: 'Pillars',
-    title: 'Earn Trust and Reputation',
-    text: 'Responsible AI benefits society and your business alike by building trust and credibility with every stakeholder.',
+    alt: "Pillars",
+    title: "Earn Trust and Reputation",
+    text: "Responsible AI benefits society and your business alike by building trust and credibility with every stakeholder.",
   },
   {
     icon: solutionsIcon,
-    alt: 'Pillars',
-    title: 'Pragmatic Ethical Solutions',
-    text: 'ALLY lays out clear, actionable steps to embed ethics in organisation-wide AI governance, drawing on others\' successes and lessons to strengthen your own strategy.',
+    alt: "Pillars",
+    title: "Pragmatic Ethical Solutions",
+    text: "ALLY lays out clear, actionable steps to embed ethics in organisation-wide AI governance, drawing on others' successes and lessons to strengthen your own strategy.",
   },
-]
-
+];
 </script>
 
 <style scoped lang="scss">
-
 .icon-container {
   width: 3rem;
   height: 3rem;
